e2499c6b5a9c3d90af49271d9ae8fc5747e8e20f
AppleJin512 <christianjin512@gmail.com>
2022-07-25 17:42:05 +0800

    refactor: 💡 remove ui

--- apps/web/src/routes/index.svelte
+++ apps/web/src/routes/index.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
   import CustomWeb3Connector from '../components/CustomWeb3Connector.svelte';
-
-  import { MyCounterButton } from 'ui';
-
 </script>
 
 <h1>Web</h1>
-<MyCounterButton />
 <CustomWeb3Connector/>
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
