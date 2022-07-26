ab0a76654bf05c54a83a46f200c200e84819c87a
AppleJin512 <christianjin512@gmail.com>
2022-07-26 08:06:44 +0800

    fix: 🐛 type error

--- packages/connect-web3-svelte/src/lib/index.ts
+++ packages/connect-web3-svelte/src/lib/index.ts
@@ -1,5 +1,5 @@
 import { isMobile, removeLocalCachedProvider } from 'connect-web3-core';
 import ConnectWeb3Modal from './ConnectWeb3Modal.svelte';
-import { connect } from "./store";
+import { connect } from './store';
 
 export { ConnectWeb3Modal, connect, isMobile, removeLocalCachedProvider };
